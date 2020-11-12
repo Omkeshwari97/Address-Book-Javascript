8c9914b0934faa26ce8441b62da29c9c933baad4
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-11-12 11:11:55 +0530

    Added: funcionality to validate contacts

--- AddressBook.js
+++ AddressBook.js
@@ -3,6 +3,13 @@ var prompt = require('prompt-sync')();
 //uc1 create contact
 let contact = new Array();
 let contactcount = 0;
+
+let regex1 = RegExp('^[A-Z]{1}[a-z]{2,}$');
+let regex2 = RegExp('^[A-za-z]{4,}$');
+let zipregex = RegExp('^[0-9]{6}$');
+let phoneregex = RegExp('^[789]{1}[0-9]{9}$');
+let emailregex = RegExp('^($|[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+)$');
+
 while(contactcount != 2)
 {
     let fname = prompt('Enter first name : ');
@@ -14,28 +21,36 @@ while(contactcount != 2)
     let phone = prompt('Enter phone number : ');
     let email = prompt('Enter email : ');
 
-    contact.push(
+    if(regex1.test(fname) && regex1.test(lname) && regex2.test(addr) && regex2.test(city) && regex2.test(state)
+    && zipregex.test(zip) && phoneregex.test(phone) && emailregex.test(email))
     {
-        firstname : fname,
-        lastname : lname,
-        address : addr,
-        cityname : city,
-        statename : state,
-        zipnumber : zip,
-        phonenumber : phone,
-        emailaddress : email,
-        toString()
-        {
-            return '\n First name : ' + this.firstname +
-            ' Last name : ' + this.lastname +
-            ' Address : ' + this.address +
-            ' City : ' + this.cityname +
-            ' State : ' + this.statename +
-            ' Zip : ' + this.zipnumber +
-            ' Phone : ' + this.phonenumber +
-            ' Email : ' + this.emailaddress 
-        }
-    });
+        contact.push(
+            {
+                firstname : fname,
+                lastname : lname,
+                address : addr,
+                cityname : city,
+                statename : state,
+                zipnumber : zip,
+                phonenumber : phone,
+                emailaddress : email,
+                toString()
+                {
+                    return '\n First name : ' + this.firstname +
+                    ' Last name : ' + this.lastname +
+                    ' Address : ' + this.address +
+                    ' City : ' + this.cityname +
+                    ' State : ' + this.statename +
+                    ' Zip : ' + this.zipnumber +
+                    ' Phone : ' + this.phonenumber +
+                    ' Email : ' + this.emailaddress 
+                }
+            });
+    }
+    else
+    {
+        throw 'Invalid input';
+    }
     contactcount++;
 }
 
