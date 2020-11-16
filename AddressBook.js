0b2ba98b0fe7555c98be0309946860302b0e9f07
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-11-16 12:16:55 +0530

    Added: functionality to create new address book array and add contacts

--- AddressBook.js
+++ AddressBook.js
@@ -10,6 +10,170 @@ let zipregex = RegExp('^[0-9]{6}$');
 let phoneregex = RegExp('^[789]{1}[0-9]{9}$');
 let emailregex = RegExp('^($|[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+)$');
 
+class Contact
+{
+    constructor(firstname, lastname, address, cityname, statename, zipnumber, phonenumber, emailaddress)
+    {
+        this.firstname = firstname
+        this.lastname = lastname;
+        this.address = address;
+        this.cityname = cityname;
+        this.statename = statename;
+        this.zipnumber = zipnumber;
+        this.phonenumber = phonenumber;
+        this.emailaddress = emailaddress;
+    }
+
+    get firstname()
+    {
+        return this._firstname;
+    }
+
+    set firstname(firstname)
+    {
+        if(regex1.test(firstname))
+        {
+            this._firstname = firstname
+        }
+        else
+        {
+            throw "First Name is Incorrect!";
+        }
+    }
+
+    get lastname()
+    {
+        return this._lastname;
+    }
+
+    set lastname(lastname)
+    {
+        if(regex1.test(lastname))
+        {
+            this._lastname = lastname;
+        }
+        else
+        {
+            throw "Last Name is Incorrect!";
+        }
+    }
+
+    get address()
+    {
+        return this._address;
+    }
+
+    set address(address)
+    {
+        if(regex2.test(address))
+        {
+            this._address = address;
+        }
+        else
+        {
+            throw "Address is Incorrect!";
+        }
+    }
+
+    get cityname()
+    {
+        return this._cityname;
+    }
+
+    set cityname(cityname)
+    {
+        if(regex2.test(cityname))
+        {
+            this._cityname = cityname;
+        }
+        else
+        {
+            throw "City is Incorrect!";
+        }
+    }
+
+    get statename()
+    {
+        return this._statename;
+    }
+
+    set statename(statename)
+    {
+        if(regex2.test(statename))
+        {
+            this._statename = statename;
+        }
+        else
+        {
+            throw "State is Incorrect!";
+        }
+    }
+
+    get zipnumber()
+    {
+        return this._zipnumber;
+    }
+
+    set zipnumber(zipnumber)
+    {
+        if(zipregex.test(zipnumber))
+        {
+            this._zipnumber = zipnumber;
+        }
+        else
+        {
+            throw "Zip is Incorrect!";
+        }
+    }
+
+    get phonenumber()
+    {
+        return this._phonenumber;
+    }
+
+    set phonenumber(phonenumber)
+    {
+        if(phoneregex.test(phonenumber))
+        {
+            this._phonenumber = phonenumber;
+        }
+        else
+        {
+            throw "Phone Number is Incorrect!";
+        }    
+    }
+
+    get emailaddress()
+    {
+        return this._emailaddress;
+    }
+
+    set emailaddress(emailaddress)
+    {
+        if(emailregex.test(emailaddress))
+        {
+            this._emailaddress = emailaddress;
+        }
+        else
+        {
+            throw "Email is Incorrect!";
+        }
+    }
+
+    toString()
+    {
+        return '\n First name : ' + this.firstname +
+        ' Last name : ' + this.lastname +
+        ' Address : ' + this.address +
+        ' City : ' + this.cityname +
+        ' State : ' + this.statename +
+        ' Zip : ' + this.zipnumber +
+        ' Phone : ' + this.phonenumber +
+        ' Email : ' + this.emailaddress 
+    }
+}
+
+
 while(contactcount != 2)
 {
     let fname = prompt('Enter first name : ');
@@ -21,36 +185,8 @@ while(contactcount != 2)
     let phone = prompt('Enter phone number : ');
     let email = prompt('Enter email : ');
 
-    if(regex1.test(fname) && regex1.test(lname) && regex2.test(addr) && regex2.test(city) && regex2.test(state)
-    && zipregex.test(zip) && phoneregex.test(phone) && emailregex.test(email))
-    {
-        contact.push(
-            {
-                firstname : fname,
-                lastname : lname,
-                address : addr,
-                cityname : city,
-                statename : state,
-                zipnumber : zip,
-                phonenumber : phone,
-                emailaddress : email,
-                toString()
-                {
-                    return '\n First name : ' + this.firstname +
-                    ' Last name : ' + this.lastname +
-                    ' Address : ' + this.address +
-                    ' City : ' + this.cityname +
-                    ' State : ' + this.statename +
-                    ' Zip : ' + this.zipnumber +
-                    ' Phone : ' + this.phonenumber +
-                    ' Email : ' + this.emailaddress 
-                }
-            });
-    }
-    else
-    {
-        throw 'Invalid input';
-    }
+    var cobj = new Contact(fname, lname, addr, city, state, zip, phone, email);
+    contact.push(cobj);
     contactcount++;
 }
 
